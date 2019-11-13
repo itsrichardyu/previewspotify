a44a3967effd1d6db5b86087c68c6deba51589f1
crayonz420 <38821021+crayonz420@users.noreply.github.com>
2019-11-12 19:52:34 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const clientId = "2041601f364e4ccc9197c572b2f911d8";
-let globalAuthToken;
+let globalAccessToken;
 
 function spotifyAuth() {
 	accessToken = window.location.hash.substr(1).split('&')[0].split("=")[1]
 	
 	if (accessToken) {
 		document.getElementById("auth").innerHTML = "Already authorized!";
-		globalAuthToken = accessToken;
+		globalAccessToken = accessToken;
 	} else if (!accessToken) {
 		const authParams = {
 			"respType": "&response_type=token",
@@ -22,11 +22,9 @@ function spotifyAuth() {
 }
 
 function refresh() {
+	fetch("https://accounts.spotify.com/api/token", {
+			method: "POST",
+			body: "grant_type=authorization_code&code=globalAccessToken&redirect_uri=https://crayonz420.github.io/spotify-chords/",
+			headers: { "Content-Type": "application/x-www-form-urlencoded" }
+		}).then(console.log("hi"))
 }
-
-fetch("https://api.spotify.com/v1/me/player/currently-playing", {
-		method: "POST",
-		headers: {
-			"Application": "Bearer" + globalAuthToken
-		},
-	}).then(console.log("hi"))
